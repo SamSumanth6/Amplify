bd08276318eb0d7966ead5ca2f4a25574dcdd33b
SamSumanth6 <sumanthvenkatesh6@gmail.com>
2024-06-23 19:31:40 +0530

    Albums

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import NavBar from './NavBar'
+import AlbumItem from './AlbumItem'
+import { albumsData } from '../assets/assets'
+import { songsData } from '../assets/assets'
 
 const Home = () => {
   return (
     <>
         <NavBar/>
+        <div className='mb-4'>
+            <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
+            <div className='flex overflow-auto'>
+            {albumsData.map((item, index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+            </div>
+        </div>
+        <div className='mb-4'>
+            <h1 className='my-5 font-bold text-2xl'>Today's biggest hits</h1>
+            <div className='flex overflow-auto'>
+            {songsData.map((item, index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+            </div>
+        </div>
     </>
   )
 }
