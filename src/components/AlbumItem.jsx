7549f60341ab8653e53cd6480f8a348a8ccd07ed
SamSumanth6 <sumanthvenkatesh6@gmail.com>
2024-06-24 10:32:35 +0530

    Final

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const AlbumItem = ({image,name,desc,id}) => {
 
     const navigate = useNavigate()
+  
 
   return (
     <div onClick={()=>navigate(`/album/${id}`)} className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import NavBar from "./NavBar";
 import { useParams } from "react-router-dom";
 import { albumsData, assets, songsData } from "../assets/assets";
+import { PlayerContext } from "../context/PlayerContext";
 
 const DisplayAlbum = () => {
   const { id } = useParams();
   const albumData = albumsData[id];
   // console.log(albumData)
+  const {playWithId} = useContext(PlayerContext)
 
   return (
     <>
@@ -39,7 +41,7 @@ const DisplayAlbum = () => {
       <hr />
       {
         songsData.map((item, index)=>(
-            <div key={index} className="grid grid-cols-2 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer ">
+            <div onClick={()=>playWithId(item.id)} key={index} className="grid grid-cols-2 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer ">
                 <p className="text-white flex items-center">
                     <b className="mr-4 text-[#a7a7a7]">{index+1}</b>
                     <img className="inline w-10 mr-5" src={item.image} alt="" />
